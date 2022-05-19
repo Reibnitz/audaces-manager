7e9564f0ce7e57e86a2ef8ce2180282c29d0d20a
Reibnitz <lucas.fpolis@gmail.com>
2022-05-19 17:49:42 -0300

    Pipe de formatação do valor de orçamento corrigiod

--- src/app/services/orcamento.pipe.ts
+++ src/app/services/orcamento.pipe.ts
@@ -5,8 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class OrcamentoPipe implements PipeTransform {
     transform(valor: number): string {
-        let regex = /(\d+)(\d{3})/;
-        // return `R$ ${valor.toLocaleString('pt-BR')}`
-        return `R$ ${valor.toFixed(2).replace('.', ',').replace(/\B(?=(\d{3})+(?!\d))/g, ".")}`;
+        return Intl.NumberFormat('pt-br', {style: 'currency', currency:'BRL'}).format(valor);
     }
 }
